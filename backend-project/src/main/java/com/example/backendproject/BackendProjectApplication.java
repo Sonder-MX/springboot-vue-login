0bf91bd3787e0c6806ea6a4212cabc48699fdd84
SonderMX <sonder-mx@outlook.com>
2023-08-13 15:57:05 +0800

    initialize backend

---
+++ backend-project/src/main/java/com/example/backendproject/BackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectApplication.class, args);
+	}
+
+}
